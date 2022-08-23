a5f945f2b3034c45d8929ea74059f877964b4cfe
Mohamad Siysinyuy <apple@apples-MacBook-Pro.local>
2022-08-23 11:58:44 +0100

    update to middle ware

--- middleware/user.js
+++ middleware/user.js
@@ -8,42 +8,52 @@ const {isValidObjectId} = require('mongoose')
 
 
 exports.isResetTokenValid = async (req,res,next)=>{
-
-  const {token, id} = req.query
-
-  if(!token || !id){
-    res.status(400).json({msg:"Invalid request1"})
-    return next(new ErrorResponse("Invalid request", 400))
-
-  }
-  if(!isValidObjectId(id)){
-    res.status(400).json({msg:"Invalid request"})
-    return next(new ErrorResponse("Invalid request", 400))
-
-  }
-  const user = await User.findById(id)
-
-  if(!user){
-    res.status(400).json({msg:"user not found"})
-    return next(new ErrorResponse("user not found", 400))
-
-  }
-
-  const resetToken = await ResetToken.findOne({owner:user._id})
-
-  if(!resetToken){
-    res.status(400).json({msg:"Reset token not found!"})
-    return next(new ErrorResponse("Reset token not found!", 400))
-
-  }
-
-    const isValid = await resetToken.compareToken(token)
-    if(!isValid){
-      res.status(400).json({msg:"Reset token is invalid!"})
-      return next(new ErrorResponse("Reset token is invalid!", 400))
+  try{
+    const {token, id} = req.query
+    if(!token || !id){
+      res.status(400).json({msg:"Invalid request1"})
+      return next(new ErrorResponse("Invalid request", 400))
 
     }
 
-    req.user = user
-    next()
+    if(!isValidObjectId(id)){
+      res.status(400).json({msg:"Invalid request"})
+      return next(new ErrorResponse("Invalid request", 400))
+
+    }
+    const user = await User.findById(id)
+    if(!user){
+      res.status(400).json({msg:"user not found"})
+      return next(new ErrorResponse("user not found", 400))
+
+    }
+
+    const resetToken = await ResetToken.findOne({owner:user._id})
+
+    if(!resetToken){
+      res.status(400).json({msg:"Reset token not found!"})
+      return next(new ErrorResponse("Reset token not found!", 400))
+
+    }
+
+      const isValid = await resetToken.compareToken(token)
+      if(!isValid){
+        res.status(400).json({msg:"Reset token is invalid!"})
+        return next(new ErrorResponse("Reset token is invalid!", 400))
+
+      }
+
+      req.user = user
+      next()
+
+  }catch(error){
+
+  }
+
+
+
+
+
+
+
 }
